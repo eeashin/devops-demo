f75f0b21191e2757935a63f81702deea8ca7857f
Matubber Mohammad Eashin <mohammadeashin.matubber@aalto.fi>
2023-11-25 20:02:02 +0100

    webhooks

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Company X application");
+  res.send("Company X++ application");
 });
 
 app.get("/feature-1", (req, res) => {
